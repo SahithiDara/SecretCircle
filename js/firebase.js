9019a185f6de3231e18277723fa8e35174b6666a
SahithiDara <50311038+SahithiDara@users.noreply.github.com>
2026-08-07 12:34:44 +0530

    Replace Firebase config with placeholders
    Updated Firebase configuration with placeholder values.

--- js/firebase.js
+++ js/firebase.js
@@ -7,12 +7,12 @@ import {
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC8-K3UGvtSAzixnpj3E6pM1x1-0eOlQKQ",
-  authDomain: "secret-circle-73b92.firebaseapp.com",
-  projectId: "secret-circle-73b92",
-  storageBucket: "secret-circle-73b92.firebasestorage.app",
-  messagingSenderId: "883032651449",
-  appId: "1:883032651449:web:a159616198f01b30740d68"
+  apiKey: "apiKey",
+  authDomain: "authDomain",
+  projectId: "projectId",
+  storageBucket: "storageBucket",
+  messagingSenderId: "messagingSenderId",
+  appId: "appId"
 };
 
 const app = initializeApp(firebaseConfig);
